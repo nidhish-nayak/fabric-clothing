427cea1ec457ed2db3f7887d81455c534a95e226
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2022-09-15 14:34:41 +0530

    project data stored in json seperately

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import './categories.styles.scss';
+import './App.css';
 import CategoryItem from './components/category-item/category-item.component';
+import data from './categoryData.json';
 
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
+  const categories = data;
 
   return (
     <div className='categories-container'>
