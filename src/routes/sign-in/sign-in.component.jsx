f308dc85abe07b00c884153248290367cf56e61f
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2022-09-19 00:48:02 +0530

    Added firebase auth with account selection option for signin with google option

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { singInWithGooglePopup } from '../../utils/firebase/firebase.utils.js';
 
 const SignIn = () => {
+
+    const logGoogleUser = async () => {
+        const response = await singInWithGooglePopup();
+        console.log(response);
+    }
+
     return (
         <div>
-            My sign in page
+            <h1>My Sing In Page: </h1>
+            <button onClick={logGoogleUser}>Sing In</button>
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,4 +18,12 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const singInWithGooglePopup = () => signInWithPopup(auth, provider);
