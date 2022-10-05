8d0c01ec00f931fcf98dcbb35289f55de0199873
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2022-10-05 11:30:29 +0530

    shop component - styles added

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SHOP_DATA from '../../data/SHOP_DATA.json';
+import './shop.styles.scss';
 
 const Shop = () => {
     return (
-        <div>
-            {SHOP_DATA.map(({ id, name }) => (
-                <div id={id}>
+        <div className="shop-container">
+            {SHOP_DATA.map(({ id, name, imageUrl }) => (
+                <div class="shop-item" id={id} style={{ backgroundImage: `url(${imageUrl})` }}>
                     <h1>{name}</h1>
                 </div>
             ))}
