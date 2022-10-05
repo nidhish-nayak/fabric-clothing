cc6b6ade79d7f70dad63bdf76e678c8ecb024627
D Nayak <nidhish.d.nayak@accenture.com>
2022-10-05 19:31:10 +0530

    Added styles to shop page

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from '../button/button.component';
+import './product-card.styles.scss';
 
 const ProductCard = ({ name, price, imageUrl }) => {
 
@@ -8,7 +9,7 @@ const ProductCard = ({ name, price, imageUrl }) => {
             <img src={imageUrl} alt="Loading" />
             <div className="footer">
                 <span className="name"> {name} </span>
-                <span className="price"> {price} </span>
+                <span className="price"> {`$${price}`} </span>
             </div>
             <Button buttonType='inverted'>Add to Cart</Button>
         </div>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import ProductCard from "../../components/product-card/product-card.component";
 import { ProductsContext } from "../../contexts/products.context";
+import './shop.styles.scss';
 
 const Shop = () => {
 
