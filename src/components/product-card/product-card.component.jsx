58460137b2ec862dd79a46645f06511a3dc11cc3
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2022-10-05 15:22:22 +0530

    shop UI component structure completed

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Button from '../button/button.component';
 
-const ProductCard = ({name, price, imageUrl}) => {
+const ProductCard = ({ name, price, imageUrl }) => {
 
     return (
         <div className="product-card-container">
-            <img src={ imageUrl } alt="Loading" />
-         <div className="footer">
-            <span className="name"> { name } </span>
-            <span className="price"> { price } </span>
-         </div>
-         <Button buttonType='inverted'>Add to Cart</Button>
+            <img src={imageUrl} alt="Loading" />
+            <div className="footer">
+                <span className="name"> {name} </span>
+                <span className="price"> {price} </span>
+            </div>
+            <Button buttonType='inverted'>Add to Cart</Button>
         </div>
     )
 }
