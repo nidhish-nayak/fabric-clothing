a98d42cba2208efade7ef75b2c7fb9f4acc5bf47
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2022-10-05 15:20:47 +0530

    shop UI component structure completed

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from '../button/button.component';
+
+const ProductCard = ({name, price, imageUrl}) => {
+
+    return (
+        <div className="product-card-container">
+            <img src={ imageUrl } alt="Loading" />
+         <div className="footer">
+            <span className="name"> { name } </span>
+            <span className="price"> { price } </span>
+         </div>
+         <Button buttonType='inverted'>Add to Cart</Button>
+        </div>
+    )
+}
+
+export default ProductCard;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import ProductCard from "../../components/product-card/product-card.component";
 import { ProductsContext } from "../../contexts/products.context";
 
 const Shop = () => {
@@ -7,10 +8,8 @@ const Shop = () => {
 
     return (
         <div className="shop-container">
-            {products.map(({ id, name }) => (
-                <div className="shop-item" key={id}>
-                    <h1>{name}</h1>
-                </div>
+            {products.map(({ id, name, price, imageUrl }) => (
+                <ProductCard key={id} name={name} price={price} imageUrl={imageUrl} />
             ))}
         </div>
     )
