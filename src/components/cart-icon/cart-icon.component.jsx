92ec86d0a84bbf0d1b002efce100e0b8059898ac
D Nayak <nidhibelthangady@gmail.com>
2022-10-14 21:19:12 +0530

    Organizing code a bit

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,25 +1,26 @@
-import { useContext } from 'react';
-import { ReactComponent as ShopIcon } from '../../assets/shopping-bag.svg';
-import { AddtocartContext } from '../../contexts/addtocart.context';
-import { CartContext } from '../../contexts/cart.context';
-import './cart-icon.styles.scss';
+import { useContext } from "react";
+import { ReactComponent as ShopIcon } from "../../assets/shopping-bag.svg";
+import { AddtocartContext } from "../../contexts/addtocart.context";
+import { CartContext } from "../../contexts/cart.context";
+import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-
     const { cartStatus, setCartStatus } = useContext(CartContext);
     const { cartContent } = useContext(AddtocartContext);
 
     return (
-        <div className="cart-icon-container" onClick={() => {
-            if (cartStatus) {
-                setCartStatus(false)
-            }
-            else setCartStatus(true);
-        }}>
+        <div
+            className="cart-icon-container"
+            onClick={() => {
+                if (cartStatus) {
+                    setCartStatus(false);
+                } else setCartStatus(true);
+            }}
+        >
             <ShopIcon className="shopping-icon" />
             <span className="item-count">{cartContent.length}</span>
         </div>
-    )
-}
+    );
+};
 
 export default CartIcon;
