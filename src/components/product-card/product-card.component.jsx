7d5270abe57759a962a6f38df1c839077549f4d1
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2022-10-14 20:21:14 +0530

    Prettier settings change

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,19 +1,18 @@
 import React, { useContext } from "react";
-import { AddtocartContext } from '../../contexts/addtocart.context';
-import Button from '../button/button.component';
-import './product-card.styles.scss';
+import { AddtocartContext } from "../../contexts/addtocart.context";
+import Button from "../button/button.component";
+import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
-
     const { id, name, price, imageUrl } = product;
     const { cartContent, setCartContent } = useContext(AddtocartContext);
 
     const addProductToCart = (newValue) => {
         // To check if the item exists in cart then do not add
-        if (!cartContent.some(i => i['id'] === newValue.id)) {
-            setCartContent([...cartContent, newValue])
+        if (!cartContent.some((i) => i["id"] === newValue.id)) {
+            setCartContent([...cartContent, newValue]);
         }
-    }
+    };
 
     return (
         <div className="product-card-container">
@@ -22,13 +21,22 @@ const ProductCard = ({ product }) => {
                 <span className="name">{name}</span>
                 <span className="price">{`$${price}`}</span>
             </div>
-            <Button buttonType='inverted' onClick={() => {
-                addProductToCart({ id: id, name: name, imageUrl: imageUrl, price: price, count: 1 })
-            }}>
+            <Button
+                buttonType="inverted"
+                onClick={() => {
+                    addProductToCart({
+                        id: id,
+                        name: name,
+                        imageUrl: imageUrl,
+                        price: price,
+                        count: 1,
+                    });
+                }}
+            >
                 Add to Cart
             </Button>
         </div>
-    )
-}
+    );
+};
 
 export default ProductCard;
