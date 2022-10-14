f078ad36feadd7c6b573bcb30d49930aba0c85bc
D Nayak <nidhibelthangady@gmail.com>
2022-10-15 00:23:13 +0530

    Combining addtocart and cart context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,15 +1,35 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
+
+const addToCartHelper = (cartItems, productToAdd) => {
+    // Find if the product is already present
+    const ifItemExists = cartItems.find((i) => i.id === productToAdd.id);
+    if (ifItemExists) {
+        // return (
+        //     cartItems.map((i) =>
+        //         i.id === productToAdd.id ? {...i,}
+        //     )
+        // )
+    }
+};
 
 export const CartContext = createContext({
     cartStatus: false,
-    setCartStatus: () => { },
-})
+    setCartStatus: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
+});
 
 export const CartProvider = ({ children }) => {
-
     const [cartStatus, setCartStatus] = useState();
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addToCartHelper(cartItems, productToAdd));
+    };
+
     const value = { cartStatus, setCartStatus };
 
-    return <CartContext.Provider value={value}>{children}</CartContext.Provider>
-
-}
+    return (
+        <CartContext.Provider value={value}>{children}</CartContext.Provider>
+    );
+};
