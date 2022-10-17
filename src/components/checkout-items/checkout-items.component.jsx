b3d69a00d65c829cb0e8820420cf72bb4322cbc9
D Nayak <nidhibelthangady@gmail.com>
2022-10-17 14:36:44 +0530

    checkout-items component created

---
+++ src/components/checkout-items/checkout-items.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./checkout-items.styles.scss";
+
+const CheckoutItems = () => {
+    return <div>My Checkout</div>;
+};
+
+export default CheckoutItems;
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import CheckoutItems from "../../components/checkout-items/checkout-items.component";
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-    return <div>Checkout Here!</div>;
+    return (
+        <div className="checkout-container">
+            <CheckoutItems className="checkout-items" />
+            <div className="total">Total: </div>
+        </div>
+    );
 };
 
 export default Checkout;
