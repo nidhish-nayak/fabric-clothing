2118935c9a04d72b3798fd4543f9d4a41dbd8c9b
D Nayak <nidhibelthangady@gmail.com>
2022-10-20 01:51:27 +0530

    created checkout page with items and styles

--- src/components/checkout-items/checkout-items.component.jsx
+++ src/components/checkout-items/checkout-items.component.jsx
@@ -2,56 +2,36 @@ import React, { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import "./checkout-items.styles.scss";
 
-const CheckoutItems = () => {
-    // use cart context for checkout page
-    const { cartItems, addItemToCart, removeItemFromCart, removeWholeItem } =
+const CheckoutItems = ({ props }) => {
+    const { name, imageUrl, price, count } = props;
+    const { addItemToCart, removeItemFromCart, removeWholeItem } =
         useContext(CartContext);
 
     return (
-        <div className="checkoutitems-container">
-            <div className="item-headings">
-                <div>Product</div>
-                <div>Description</div>
-                <div>Quantity</div>
-                <div>Price</div>
-                <div>Remove</div>
+        <div className="checkout-item-container">
+            <div className="image-container">
+                <img src={imageUrl} alt={`${name}`} />
             </div>
-            {cartItems.map((i) => (
-                <div className="checkoutitems" key={i.id}>
-                    <img
-                        className="item-image"
-                        src={i.imageUrl}
-                        alt="alternate img"
-                    />
-                    <div className="item-name">{i.name}</div>
-                    <div
-                        className="item-arrow"
-                        onClick={() => {
-                            removeItemFromCart(i);
-                        }}
-                    >
-                        &lt;
-                    </div>
-                    <div className="item-count">{i.count}</div>
-                    <div
-                        className="item-arrow"
-                        onClick={() => {
-                            addItemToCart(i);
-                        }}
-                    >
-                        &gt;
-                    </div>
-                    <div className="item-price">{i.price}</div>
-                    <div
-                        className="item-remove"
-                        onClick={() => {
-                            removeWholeItem(i);
-                        }}
-                    >
-                        X
-                    </div>
+            <span className="name">{name}</span>
+            <span className="quantity">
+                <div
+                    className="arrow"
+                    onClick={() => removeItemFromCart(props)}
+                >
+                    &#10094;
                 </div>
-            ))}
+                <span className="value">{count}</span>
+                <div className="arrow" onClick={() => addItemToCart(props)}>
+                    &#10095;
+                </div>
+            </span>
+            <span className="price">{price}</span>
+            <div
+                className="remove-button"
+                onClick={() => removeWholeItem(props)}
+            >
+                &#10005;
+            </div>
         </div>
     );
 };
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -6,16 +6,35 @@ import "./checkout.styles.scss";
 const Checkout = () => {
     const { cartItems } = useContext(CartContext);
     return (
-        <div className="checkout">
-            <CheckoutItems />
-            <div className="total">
+        <div className="checkout-container">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
+            {cartItems.map((i) => (
+                <CheckoutItems key={i.id} props={i} />
+            ))}
+            <span className="total">
                 Total:
                 {cartItems.reduce(
                     (total, cartItems) =>
                         total + cartItems.count * cartItems.price,
                     0
                 )}
-            </div>
+            </span>
         </div>
     );
 };
