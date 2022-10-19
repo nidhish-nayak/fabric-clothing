228693d6e23e8d42b005afb947e1753a1eadb8da
D Nayak <nidhibelthangady@gmail.com>
2022-10-19 10:14:16 +0530

    Removed the extra logo file

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,14 +1,17 @@
 import { createContext, useState } from "react";
-import SHOP_DATA from '../data/SHOP_DATA.json';
+import SHOP_DATA from "../data/SHOP_DATA.json";
 
 export const ProductsContext = createContext({
-    products: []
+    products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-
     const [products] = useState(SHOP_DATA);
     const value = { products };
 
-    return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
-}
+    return (
+        <ProductsContext.Provider value={value}>
+            {children}
+        </ProductsContext.Provider>
+    );
+};
