0550366a234764a2cdb571f7f1f82ab0c7bf8418
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2022-10-22 01:06:43 +0530

    Checking error

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import ProductCard from "../../components/product-card/product-card.component";
 import { CategoriesContext } from "../../contexts/categories.context";
 import "./shop.styles.scss";
