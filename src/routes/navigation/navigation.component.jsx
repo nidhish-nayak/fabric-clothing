f01303a3f0a3e667107e1949d979d34e77783e9e
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-02-01 21:01:56 +0530

    Navbar updates to signout on signin with Redux using useSelector hook

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,10 @@
 import React, { Fragment, useContext } from "react"; //Fragment is used if no parent div element is needed
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component.jsx";
 import CartIcon from "../../components/cart-icon/cart-icon.component.jsx";
 import { CartContext } from "../../contexts/cart.context";
-import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import {
     LogoContainer,
@@ -14,7 +14,8 @@ import {
 } from "./navigation.styles.jsx";
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
+    const currentUser = useSelector((state) => state.user.currentUser);
+    // const { currentUser } = useContext(UserContext);
     const { cartStatus } = useContext(CartContext);
 
     return (
