7bbc8f89e04478a49a6b46262827df1e896654ab
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-02-05 22:56:13 +0530

    Initializing CategoriesContext migration to Redux

---
+++ src/store/category/category.action.js
@@ -0,0 +1,4 @@
+import { createAction } from "../../utils/reducer/reducer.utils";
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
+
+export const setCategoriesMap = (categoriesMap) => createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesMap);
