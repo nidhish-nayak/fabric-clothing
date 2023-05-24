5d942ce9e1c0509288336366f0d97172647bdfe2
D Nayak <nidhish.d.nayak@accenture.com>
2023-05-24 23:09:33 +0530

    Added a comment on selector - Login migrated from firebase.util.js to categoriesSelector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,10 @@
-export const categoriesSelector = (state) => state.categories.categories
-    .reduce((acc, category) => {
-        const { title, items } = category;
-        acc[title.toLowerCase()] = items;
-        console.log(acc);
-        return acc;
-    }, {});
+// This logic was migrated here from firebase.utils.js since we will be using selectors to modify data as we want for the components
+export const categoriesSelector = (state) => {
+    return state.categories.categories
+        .reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            console.log(acc);
+            return acc;
+        }, {});
+}
