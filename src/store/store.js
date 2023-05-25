b67224cd0b46a58cd319aeefae684a079ab93d8e
D Nayak <nidhish.d.nayak@accenture.com>
2023-05-25 14:38:41 +0530

    Created our own logger middleware in store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,26 @@
 import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
+// Creating our own middleware [logger]
+const loggerMiddleware = (store) => (next) => (action) => {
+    // In case actions received with no types (Redux Thunk)
+    if (!action.type) {
+        return next(action);
+    }
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState', store.getState());
+
+    next(action);
+
+    // Next state will be only available post running the reducer. Hence, printing the state after next
+    console.log('next state: ', store.getState());
+}
+
 // Adding middlewares and combining them using compose method
-const middleWares = [logger];
-const composeEnhancers = compose(applyMiddleware(...middleWares));
+// const middleWares = [logger];
+const composeEnhancers = compose(applyMiddleware(...loggerMiddleware));
 
 // createStore with rootReducer and Middlewares - undefined is just optional syntax
 export const store = createStore(rootReducer, undefined, composeEnhancers);
