44379c71d4d69f8bb489b36cc80e133ffaef9716
D Nayak <nidhish.d.nayak@accenture.com>
2023-05-30 18:09:51 +0530

    Starting footer section

---
+++ src/components/footer/footer.styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.section`
+    margin: 0;
+    height: 20em;
+    background-color: #e8e8e8;
+    font-family: "Amulya", sans-serif;
+`;
