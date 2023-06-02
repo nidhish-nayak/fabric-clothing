a9afbfd2c8d5523c2696b801291005d34c698e81
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-06-02 16:33:52 +0530

    Redux - Cart initialized

---
+++ src/store/cart/cart.reducer.js
@@ -0,0 +1,26 @@
+import { CART_ACTION_TYPES } from "./cart.types";
+
+export const CART_INITIAL_STATE = {
+    isCartOpen: false,
+    cartItems: [],
+    cartCount: 0,
+    cartTotal: 0,
+};
+
+export const cartReducer = (state = CART_INITIAL_STATE, action = {}) => {
+    const { type, payload } = action;
+    switch (type) {
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload,
+            };
+        case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+            return {
+                ...state,
+                ...payload,
+            };
+        default:
+            throw new Error(`Unhandled type ${type} in the userReducer !`);
+    }
+};
