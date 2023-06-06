a4817774bf924dec285f089be89685a8ba62e3f4
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-06-06 19:35:44 +0530

    Implemented Redux for Cart component - Test Required

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { CartProvider } from './contexts/cart.context';
-import './index.scss';
-import reportWebVitals from './reportWebVitals';
-import { store } from './store/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        {/* <UserProvider> */}
-        {/* <CategoriesProvider> */}
-        <CartProvider>
-          <App />
-        </CartProvider>
-        {/* </CategoriesProvider> */}
-        {/* </UserProvider> */}
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                {/* <UserProvider> */}
+                {/* <CategoriesProvider> */}
+                {/* <CartProvider> */}
+                <App />
+                {/* </CartProvider> */}
+                {/* </CategoriesProvider> */}
+                {/* </UserProvider> */}
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/store/cart/cart.reducer.js
+++ src/store/cart/cart.reducer.js
@@ -3,8 +3,6 @@ import { CART_ACTION_TYPES } from "./cart.types";
 export const CART_INITIAL_STATE = {
     isCartOpen: false,
     cartItems: [],
-    cartCount: 0,
-    cartTotal: 0,
 };
 
 export const cartReducer = (state = CART_INITIAL_STATE, action = {}) => {
@@ -14,8 +12,15 @@ export const cartReducer = (state = CART_INITIAL_STATE, action = {}) => {
         case CART_ACTION_TYPES.SET_CART_ITEMS:
             return {
                 ...state,
-                ...payload,
+                cartItems: payload,
             };
+
+        case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+            return {
+                ...state,
+                isCartOpen: payload,
+            };
+
         default:
             return state;
     }
