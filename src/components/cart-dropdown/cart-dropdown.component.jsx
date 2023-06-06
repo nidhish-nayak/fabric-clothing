7f4afde8a5eaa17c4b2037c10cdc83462549abe5
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-06-06 23:59:53 +0530

    Adding useSelector to cart-item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -12,6 +12,7 @@ import {
 import "./cart-dropdown.styles.jsx";
 
 const CartDropdown = () => {
+
     const cartItems = useSelector(selectCartItems);
     const navigate = useNavigate();
 
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectCartItems } from "../../store/cart/cart.selector";
 import "./cart-item.styles";
 import {
     CartItemContainer,
@@ -7,8 +9,11 @@ import {
     Name,
 } from "./cart-item.styles";
 
-const CartItem = ({ cartItem }) => {
+const CartItem = () => {
+
+    const cartItem = useSelector(selectCartItems);
     const { imageUrl, name, price, quantity } = cartItem;
+
     return (
         <CartItemContainer>
             <ItemImage src={imageUrl} alt="alternative" />
