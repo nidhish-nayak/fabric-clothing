e0a9ca398b0fb7f19f692544eb2fbc5d7027e4d9
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-06-22 14:48:09 +0530

    Initializing Redux Toolkit - yarn add @reduxjs/toolkit performed

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import {
-    applyMiddleware,
-    compose,
-    legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
 } from "redux";
 import logger from "redux-logger";
 import { persistReducer, persistStore } from "redux-persist";
@@ -10,23 +10,23 @@ import thunk from "redux-thunk";
 import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
-    key: "root",
-    storage,
-    whitelist: ["cart"],
+  key: "root",
+  storage,
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
-    process.env.NODE_ENV !== "production" && logger,
-    thunk, // Added Redux Thunk as middleware
+  process.env.NODE_ENV !== "production" && logger,
+  thunk, // Added Redux Thunk as middleware
 ].filter(Boolean);
 
 const composeEnhancer =
-    (process.env.NODE_ENV !== "production" &&
-        window &&
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-    compose;
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
