ee0d4d14b247ce18a3e175b768a1407af82d5b50
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-06-29 15:05:43 +0530

    Added payment error alerts for success and failure

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -25,7 +25,27 @@ const PaymentForm = () => {
       body: JSON.stringify({ amount: 10000 })
     }).then((res) => res.json())
 
-    console.log(response);
+    const { paymentIntent: { client_secret } } = response;
+
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Nidhish"
+        }
+      }
+    })
+
+    if (paymentResult.error) {
+      alert(paymentResult.error)
+    }
+    else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment succeeded')
+      }
+    }
 
   }
 
