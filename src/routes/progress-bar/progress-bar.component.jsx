556c6bebe9cc5cc2dcf576009f578013b54bda7e
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-08 00:13:40 +0530

    Removed scroll bar disabled for other animation issues

--- src/routes/progress-bar/progress-bar.component.jsx
+++ src/routes/progress-bar/progress-bar.component.jsx
@@ -6,7 +6,6 @@ const ProgressBar = () => {
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    document.body.style.overflow = 'hidden'; // Disable scrolling
     const interval = setInterval(() => {
       if (progress < 100) {
         setProgress((prevProgress) => prevProgress + 1);
@@ -21,7 +20,6 @@ const ProgressBar = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  progress <= 150 ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto';
   const text = progress <= 100 ? `${progress}%` : '100%';
 
   return (
