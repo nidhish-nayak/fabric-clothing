bd0617682873ddc9b2b2f5fe674ba49c6b389f15
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-07 23:58:12 +0530

    added balancer around directory container

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Balancer } from "react-wrap-balancer";
+
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import {
     ButtonContainer, DirectoryContainer, GridContainer, SectionContainer,
@@ -42,25 +44,27 @@ const Directory = () => {
     }
 
     return (
-        <DirectoryContainer>
-            <SectionContainer>
-                <TitleContainer>
-                    <SectionTitle>Unleash your style with Fabric Clothing</SectionTitle>
-                    <SectionDesc>
-                        Get ready to ignite your style and redefine what it means to dress with confidence.
-                        Step into a world where fashion knows no boundaries with our exceptional clothing collection.
-                    </SectionDesc>
-                    <ButtonContainer>
-                        <Button onClick={onShopHandler}>SHOP NOW</Button>
-                        <Button onClick={onAuthHandler} buttonType={BUTTON_TYPE_CLASSES.inverted}>SIGN UP</Button>
-                    </ButtonContainer>
-                </TitleContainer>
-                <SectionItem src="https://i.imgur.com/LssQ1lH.png" />
-            </SectionContainer>
-            <GridContainer>
-                {trendingItems.map(i => <img key={i.id} src={i.imageUrl} alt="alt" />)}
-            </GridContainer>
-        </DirectoryContainer>
+        <Balancer>
+            <DirectoryContainer>
+                <SectionContainer>
+                    <TitleContainer>
+                        <SectionTitle>Unleash your style with Fabric Clothing</SectionTitle>
+                        <SectionDesc>
+                            Get ready to ignite your style and redefine what it means to dress with confidence.
+                            Step into a world where fashion knows no boundaries with our exceptional clothing collection.
+                        </SectionDesc>
+                        <ButtonContainer>
+                            <Button onClick={onShopHandler}>SHOP NOW</Button>
+                            <Button onClick={onAuthHandler} buttonType={BUTTON_TYPE_CLASSES.inverted}>SIGN UP</Button>
+                        </ButtonContainer>
+                    </TitleContainer>
+                    <SectionItem src="https://i.imgur.com/LssQ1lH.png" />
+                </SectionContainer>
+                <GridContainer>
+                    {trendingItems.map(i => <img key={i.id} src={i.imageUrl} alt="alt" />)}
+                </GridContainer>
+            </DirectoryContainer>
+        </Balancer>
     );
 };
 
