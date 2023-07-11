49abfb57df69726bd3074dc481cd97651d61c702
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-12 00:46:20 +0530

    Added image links for grid

--- src/components/grid-section/grid-section.component.jsx
+++ src/components/grid-section/grid-section.component.jsx
@@ -9,31 +9,31 @@ const gridCategories = [
   {
     id: 1,
     title: "hats",
-    imageUrl: "https://i.imgur.com/DjRwg8f.jpg",
+    imageUrl: "https://i.ibb.co/TvqVc28/Escape.webp",
     route: "shop/hats",
   },
   {
     id: 2,
     title: "jackets",
-    imageUrl: "https://i.imgur.com/bOs7PIE.jpg",
+    imageUrl: "https://i.ibb.co/h1F4yvr/Olive-Faux-Jacket.webp",
     route: "shop/jackets",
   },
   {
     id: 3,
     title: "sneakers",
-    imageUrl: "https://i.imgur.com/b2HnocH.jpg",
+    imageUrl: "https://i.ibb.co/sVCCkBZ/Jordan-Max-Aura-4.webp",
     route: "shop/sneakers",
   },
   {
     id: 4,
     title: "womens",
-    imageUrl: "https://i.imgur.com/atYy6yP.jpg",
+    imageUrl: "https://i.ibb.co/TBxrTQh/Directory-2.webp",
     route: "shop/womens",
   },
   {
     id: 5,
     title: "mens",
-    imageUrl: "https://i.imgur.com/DsMhPk5.jpg",
+    imageUrl: "https://i.ibb.co/7KMFZHy/Directory-1.webp",
     route: "shop/mens",
   },
 ];
