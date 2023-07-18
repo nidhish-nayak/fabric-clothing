753fcd2cdd09ced1dab252a79376525074404e16
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-19 00:03:27 +0530

    Store.js updated to TS as per Redux Toolkit docs

--- src/store/store.ts
+++ src/store/store.ts
@@ -22,3 +22,8 @@ export const store = configureStore({
 });
 
 export const persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
