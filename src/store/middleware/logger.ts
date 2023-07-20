512e0852b843105abc2c8b1f9961c7d572f2fb3f
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-21 01:49:12 +0530

    Middleware - logger.js converted to logger.ts with typed import

---
+++ src/store/middleware/logger.ts
@@ -0,0 +1,18 @@
+import { Middleware } from "redux";
+
+export const loggerMiddleware: Middleware = (store) => (next) => (action) => {
+	// In case actions received with no types (Redux Thunk)
+	if (!action.type) {
+		return next(action);
+	}
+
+	console.group("Logger Output: ");
+	console.log("Type: ", action.type);
+	console.log("Payload: ", action.payload);
+	console.log("Current State: ", store.getState());
+
+	next(action);
+
+	console.table("Next State: ", store.getState());
+	console.groupEnd();
+};
