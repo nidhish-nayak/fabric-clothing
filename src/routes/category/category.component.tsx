684bf85a2bf958b046ca2dafa7f32bfaec5485db
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-30 23:41:48 +0530

    Typescript implemented for routes/category components

---
+++ src/routes/category/category.component.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import Footer from "../../components/footer/footer.component";
+import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
+
+import {
+	selectCategoriesIsLoading,
+	selectCategoriesMap,
+} from "../../store/categories/categories.selector";
+
+import { Items } from "../../store/categories/categories.types";
+import {
+	CategoryContainer,
+	CategorySection,
+	CategoryTitle,
+} from "./category.styles";
+
+type CategoryRouteParams = {
+	category: string;
+};
+
+const Category = () => {
+	// useParams - Dynamic url routing
+	const { category } = useParams<
+		keyof CategoryRouteParams
+	>() as CategoryRouteParams;
+	const categoriesMap = useSelector(selectCategoriesMap);
+	const isLoading = useSelector(selectCategoriesIsLoading);
+	const [products, setProducts] = useState<Items[]>();
+
+	useEffect(() => {
+		setProducts(categoriesMap[category]);
+	}, [category, categoriesMap]);
+
+	return (
+		<CategorySection>
+			<CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<CategoryContainer>
+					{products &&
+						products.map((i) => <ProductCard key={i.id} product={i} />)}
+				</CategoryContainer>
+			)}
+			<Footer />
+		</CategorySection>
+	);
+};
+
+export default Category;
