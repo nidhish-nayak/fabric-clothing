1d37cf72c57393f0a6d6fbbbb1fea563cc4a78e1
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-08-17 22:39:03 +0530

    Removed extra border-radius on Grid items in home page

--- src/components/directory-item/directory-item.styles.tsx
+++ src/components/directory-item/directory-item.styles.tsx
@@ -52,8 +52,6 @@ export const PrimaryContainer = styled.div`
 	height: 700px;
 	margin: 2em 2px 2px 2px;
 	border-radius: 2px;
-	border-top-left-radius: 10px;
-	border-top-right-radius: 10px;
 	display: flex;
 	flex: 1 1 auto;
 	align-items: center;
@@ -107,8 +105,6 @@ export const SecondaryContainer = styled.div`
 	width: 100%;
 	margin: 2px;
 	border-radius: 2px;
-	border-bottom-left-radius: 10px;
-	border-bottom-right-radius: 10px;
 	height: 700px;
 	display: flex;
 	align-items: center;
