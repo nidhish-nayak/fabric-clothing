9fcf5dbf96414312b1c7635b0049abf4f9a60b14
Nidhish <76598208+nidhish-nayak@users.noreply.github.com>
2023-09-25 17:23:39 +0000

    Button tests added for base-button, google-button and isLoading state of button element

---
+++ src/components/button/__tests__/button.component.test.js
@@ -0,0 +1,26 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Button, { BUTTON_TYPE_CLASSES } from "../button.component";
+
+describe("button tests", () => {
+	test("should render base button when nothing is passed", () => {
+		render(<Button>Test</Button>);
+
+		const buttonElement = screen.getByText("Test");
+		expect(buttonElement).toHaveStyle("background-color: black");
+	});
+
+	test("should render google button when passed google button type", () => {
+		render(<Button buttonType={BUTTON_TYPE_CLASSES.google} />);
+
+		const buttonElement = screen.getByRole("button");
+		expect(buttonElement).toHaveStyle("background-color: #4285f4");
+	});
+
+	test("should be disabled if isLoading is true", () => {
+		render(<Button isLoading={true} />);
+
+		const buttonElement = screen.getByRole("button");
+		expect(buttonElement).toBeDisabled();
+	});
+});
